ae59a10e441632d24dc48b655b090dac618ac347
S-ThiagoAndre.San <s-thiagoandre.san@lwtech.edu>
2026-01-12 22:23:53 -0800

    keep correcting

--- feed.py
+++ feed.py
@@ -29,8 +29,10 @@ for item in yaml_data['item']:
     xml_tree.SubElement(item_element, 'title').text = item['title']
     xml_tree.SubElement(channel_element, 'author').text = yaml_data['author']
     xml_tree.SubElement(channel_element, 'description').text = yaml_data['description']
-    xml_tree.SubElement(channel_element, 'duration').text = yaml_data['duration']
     xml_tree.SubElement(channel_element, 'pubDate').text = yaml_data['published']
+    xml_tree.SubElement(channel_element, 'fiel').text = yaml_data['file']
+    xml_tree.SubElement(channel_element, 'duration').text = yaml_data['duration']
+    xml_tree.SubElement(channel_element, 'length').text = yaml_data['length']
 
     enclosure = xml_tree.SubElement(item_element, 'enclosure', 
                                     {'url':link_prefix + item['file'], 
